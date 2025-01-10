e2e25a23b3feb66a073374fd5e87bdec4f40afe2
SAMUEL CHIBUNNA <info@patooworld.com>
2025-01-10 06:19:02 -0800

    Update utils.ts
    fix issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,89 +1,138 @@
-import { clsx, type ClassValue } from 'clsx'
-import { customAlphabet } from 'nanoid'
-import { twMerge } from 'tailwind-merge'
+import {
+  CoreMessage,
+  CoreToolMessage,
+  generateId,
+  Message,
+  ToolInvocation,
+} from "ai";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { Chat } from "@/db/schema";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  7
-) // 7-character random string
+interface ApplicationError extends Error {
+  info: string;
+  status: number;
+}
 
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
-  const res = await fetch(input, init)
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
 
   if (!res.ok) {
-    const json = await res.json()
-    if (json.error) {
-      const error = new Error(json.error) as Error & {
-        status: number
-      }
-      error.status = res.status
-      throw error
-    } else {
-      throw new Error('An unexpected error occurred')
+    const error = new Error(
+      "An error occurred while fetching the data.",
+    ) as ApplicationError;
+
+    error.info = await res.json();
+    error.status = res.status;
+
+    throw error;
+  }
+
+  return res.json();
+};
+
+export function getLocalStorage(key: string) {
+  if (typeof window !== "undefined") {
+    return JSON.parse(localStorage.getItem(key) || "[]");
+  }
+  return [];
+}
+
+export function generateUUID(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+function addToolMessageToChat({
+  toolMessage,
+  messages,
+}: {
+  toolMessage: CoreToolMessage;
+  messages: Array<Message>;
+}): Array<Message> {
+  return messages.map((message) => {
+    if (message.toolInvocations) {
+      return {
+        ...message,
+        toolInvocations: message.toolInvocations.map((toolInvocation) => {
+          const toolResult = toolMessage.content.find(
+            (tool) => tool.toolCallId === toolInvocation.toolCallId,
+          );
+
+          if (toolResult) {
+            return {
+              ...toolInvocation,
+              state: "result",
+              result: toolResult.result,
+            };
+          }
+
+          return toolInvocation;
+        }),
+      };
     }
+
+    return message;
+  });
+}
+
+export function convertToUIMessages(
+  messages: Array<CoreMessage>,
+): Array<Message> {
+  return messages.reduce((chatMessages: Array<Message>, message) => {
+    if (message.role === "tool") {
+      return addToolMessageToChat({
+        toolMessage: message as CoreToolMessage,
+        messages: chatMessages,
+      });
+    }
+
+    let textContent = "";
+    let toolInvocations: Array<ToolInvocation> = [];
+
+    if (typeof message.content === "string") {
+      textContent = message.content;
+    } else if (Array.isArray(message.content)) {
+      for (const content of message.content) {
+        if (content.type === "text") {
+          textContent += content.text;
+        } else if (content.type === "tool-call") {
+          toolInvocations.push({
+            state: "call",
+            toolCallId: content.toolCallId,
+            toolName: content.toolName,
+            args: content.args,
+          });
+        }
+      }
+    }
+
+    chatMessages.push({
+      id: generateId(),
+      role: message.role,
+      content: textContent,
+      toolInvocations,
+    });
+
+    return chatMessages;
+  }, []);
+}
+
+export function getTitleFromChat(chat: Chat) {
+  const messages = convertToUIMessages(chat.messages as Array<CoreMessage>);
+  const firstMessage = messages[0];
+
+  if (!firstMessage) {
+    return "Untitled";
   }
 
-  return res.json()
-}
-
-export function formatDate(input: string | number | Date): string {
-  const date = new Date(input)
-  return date.toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
-  })
-}
-
-export const formatNumber = (value: number) =>
-  new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  }).format(value)
-
-export const runAsyncFnWithoutBlocking = (
-  fn: (...args: any) => Promise<any>
-) => {
-  fn()
-}
-
-export const sleep = (ms: number) =>
-  new Promise(resolve => setTimeout(resolve, ms))
-
-export const getStringFromBuffer = (buffer: ArrayBuffer) =>
-  Array.from(new Uint8Array(buffer))
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('')
-
-export enum ResultCode {
-  InvalidCredentials = 'INVALID_CREDENTIALS',
-  InvalidSubmission = 'INVALID_SUBMISSION',
-  UserAlreadyExists = 'USER_ALREADY_EXISTS',
-  UnknownError = 'UNKNOWN_ERROR',
-  UserCreated = 'USER_CREATED',
-  UserLoggedIn = 'USER_LOGGED_IN'
-}
-
-export const getMessageFromCode = (resultCode: string) => {
-  switch (resultCode) {
-    case ResultCode.InvalidCredentials:
-      return 'Invalid credentials!'
-    case ResultCode.InvalidSubmission:
-      return 'Invalid submission, please try again!'
-    case ResultCode.UserAlreadyExists:
-      return 'User already exists, please log in!'
-    case ResultCode.UserCreated:
-      return 'User created, welcome!'
-    case ResultCode.UnknownError:
-      return 'Something went wrong, please try again!'
-    case ResultCode.UserLoggedIn:
-      return 'Logged in!'
-  }
+  return firstMessage.content;
 }
